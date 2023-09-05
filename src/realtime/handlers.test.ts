3e8bc40df517d242ddfa1657cb97cb23ce8397e4
Matt Nemitz <matt.nemitz@gmail.com>
2023-09-05 14:43:03 +0100

    Replace Rome with Biome (#4)
    The Rome tools project has been abandoned in favour of the community fork Biome: https://biomejs.dev/blog/annoucing-biome

--- src/realtime/handlers.test.ts
+++ src/realtime/handlers.test.ts
@@ -101,11 +101,13 @@ describe('RealtimeSocketHandler', () => {
       reason: 'Error occurred',
       type: 'data_error',
     };
+    // rome-ignore lint/complexity/useLiteralKeys: lets us test the private methods
     realtimeSocketHandler['onSocketMessage'](error);
     expect(mockSubscriber.onError).toHaveBeenCalledWith(error);
   });
 
   test('onSocketDisconnect', () => {
+    // rome-ignore lint/complexity/useLiteralKeys: lets us test the private methods
     realtimeSocketHandler['onSocketDisconnect']();
     expect(mockSubscriber.onDisconnect).toHaveBeenCalled();
   });
@@ -116,6 +118,7 @@ describe('RealtimeSocketHandler', () => {
       reason: 'Error occurred',
       type: 'data_error',
     };
+    // rome-ignore lint/complexity/useLiteralKeys: lets us test the private methods
     realtimeSocketHandler['onSocketError'](error);
     expect(mockSubscriber.onError).toHaveBeenCalledWith(error);
   });
