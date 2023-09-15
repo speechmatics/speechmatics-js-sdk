644dacd0e869662ceed85814ac69d3526028d999
Tudor Evans <tudore@speechmatics.com>
2023-09-15 13:22:18 +0100

    formatting fixes

--- examples/microphone_nextjs/pages/index.tsx
+++ examples/microphone_nextjs/pages/index.tsx
@@ -1,25 +1,25 @@
-import { GetServerSideProps } from "next";
+import { GetServerSideProps } from 'next';
 import React, {
   useState,
   useMemo,
   useRef,
   CSSProperties,
   useEffect,
-} from "react";
-import { RealtimeSession, RealtimeRecognitionResult } from "speechmatics";
-import { AudioRecorder, useAudioDevices } from "../utils/recorder";
+} from 'react';
+import { RealtimeSession, RealtimeRecognitionResult } from 'speechmatics';
+import { AudioRecorder, useAudioDevices } from '../utils/recorder';
 import { getJwt } from '../utils/auth';
 
 type MainProps = { jwt?: string };
 
-type SessionState = "configure" | "starting" | "blocked" | "error" | "running";
+type SessionState = 'configure' | 'starting' | 'blocked' | 'error' | 'running';
 
 export default function Main({ jwt }: MainProps) {
   const [transcription, setTranscription] = useState<
     RealtimeRecognitionResult[]
   >([]);
-  const [audioDeviceId, setAudioDeviceId] = useState<string>("");
-  const [sessionState, setSessionState] = useState<SessionState>("configure");
+  const [audioDeviceId, setAudioDeviceId] = useState<string>('');
+  const [sessionState, setSessionState] = useState<SessionState>('configure');
 
   const rtSessionRef = useRef(new RealtimeSession(jwt));
 
@@ -35,7 +35,7 @@ export default function Main({ jwt }: MainProps) {
       !devices.some((item) => item.deviceId === audioDeviceId)
     )
       setAudioDeviceId(devices[0].deviceId);
-    if (denied) setSessionState("blocked");
+    if (denied) setSessionState('blocked');
   }, [devices, denied]);
 
   // sendAudio is used as a wrapper for the websocket to check the socket is finished init-ing before sending data
@@ -52,39 +52,40 @@ export default function Main({ jwt }: MainProps) {
   const audioRecorder = useMemo(() => new AudioRecorder(sendAudio), []);
 
   // Attach our event listeners to the realtime session
-  rtSessionRef.current.addListener("AddTranscript", (res) => {
+  rtSessionRef.current.addListener('AddTranscript', (res) => {
     setTranscription([...transcription, ...res.results]);
   });
 
   // start audio recording once the websocket is connected
-  rtSessionRef.current.addListener("RecognitionStarted", async () => {
-    setSessionState("running");
+  rtSessionRef.current.addListener('RecognitionStarted', async () => {
+    setSessionState('running');
   });
 
-  rtSessionRef.current.addListener("EndOfTranscript", async () => {
-    setSessionState("configure");
+  rtSessionRef.current.addListener('EndOfTranscript', async () => {
+    setSessionState('configure');
     await audioRecorder.stopRecording();
   });
 
-  rtSessionRef.current.addListener("Error", async () => {
-    setSessionState("error");
+  rtSessionRef.current.addListener('Error', async () => {
+    setSessionState('error');
     await audioRecorder.stopRecording();
   });
 
   // Call the start method on click to start the websocket
   const startTranscription = async () => {
-    setSessionState("starting");
-    await audioRecorder.startRecording(audioDeviceId)
-    .then(async () => {
-      setTranscription([]);
-      await rtSessionRef.current.start({
-        transcription_config: { max_delay: 2, language: "en" },
-        audio_format: {
-          type: "file",
-        },
-      });
-
-    }).catch(err => setSessionState("blocked"))
+    setSessionState('starting');
+    await audioRecorder
+      .startRecording(audioDeviceId)
+      .then(async () => {
+        setTranscription([]);
+        await rtSessionRef.current.start({
+          transcription_config: { max_delay: 2, language: 'en' },
+          audio_format: {
+            type: 'file',
+          },
+        });
+      })
+      .catch((err) => setSessionState('blocked'));
   };
 
   // Stop the transcription on click to end the recording
@@ -95,10 +96,10 @@ export default function Main({ jwt }: MainProps) {
 
   return (
     <div>
-      <div className="flex-row">
+      <div className='flex-row'>
         <p>Select Microphone</p>
-        {sessionState === "blocked" && (
-          <p className="warning-text">Microphone permission is blocked</p>
+        {sessionState === 'blocked' && (
+          <p className='warning-text'>Microphone permission is blocked</p>
         )}
       </div>
       <MicSelect
@@ -116,18 +117,18 @@ export default function Main({ jwt }: MainProps) {
         stopTranscription={stopTranscription}
         startTranscription={startTranscription}
       />
-      {sessionState === "error" && (
-        <p className="warning-text">Session encountered an error</p>
+      {sessionState === 'error' && (
+        <p className='warning-text'>Session encountered an error</p>
       )}
-      {["starting", "running", "configure", "blocked"].includes(
-        sessionState
+      {['starting', 'running', 'configure', 'blocked'].includes(
+        sessionState,
       ) && <p>State: {sessionState}</p>}
       <p>
         {transcription.map(
           (item, index) =>
-            (index && ![".", ","].includes(item.alternatives[0].content)
-              ? " "
-              : "") + item.alternatives[0].content
+            (index && !['.', ','].includes(item.alternatives[0].content)
+              ? ' '
+              : '') + item.alternatives[0].content,
         )}
       </p>
     </div>
@@ -140,7 +141,7 @@ export default function Main({ jwt }: MainProps) {
 // This ensures the security of long-lived tokens and reduces the scope for abuse from end users
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const jwt = await getJwt();
-  if (jwt === undefined) throw new Error("JWT undefined");
+  if (jwt === undefined) throw new Error('JWT undefined');
   return {
     props: { jwt },
   };
@@ -160,30 +161,30 @@ function TranscriptionButton({
   sessionState,
 }: TranscriptionButtonProps) {
   return (
-    <div className="bottom-button-status">
-      {["configure", "stopped", "starting", "error", "blocked"].includes(
-        sessionState
+    <div className='bottom-button-status'>
+      {['configure', 'stopped', 'starting', 'error', 'blocked'].includes(
+        sessionState,
       ) && (
         <button
-          type="button"
-          className="bottom-button start-button"
-          disabled={["starting", "blocked"].includes(sessionState)}
+          type='button'
+          className='bottom-button start-button'
+          disabled={['starting', 'blocked'].includes(sessionState)}
           onClick={async () => {
             startTranscription();
           }}
         >
-          <CircleIcon style={{ marginRight: "0.25em", marginTop: "1px" }} />
+          <CircleIcon style={{ marginRight: '0.25em', marginTop: '1px' }} />
           Start Transcribing
         </button>
       )}
 
-      {sessionState === "running" && (
+      {sessionState === 'running' && (
         <button
-          type="button"
-          className="bottom-button stop-button"
+          type='button'
+          className='bottom-button stop-button'
           onClick={() => stopTranscription()}
         >
-          <SquareIcon style={{ marginRight: "0.25em", marginBottom: "1px" }} />
+          <SquareIcon style={{ marginRight: '0.25em', marginBottom: '1px' }} />
           Stop Transcribing
         </button>
       )}
@@ -195,20 +196,20 @@ function CircleIcon(props: React.SVGProps<SVGSVGElement> & CSSProperties) {
   return (
     <span style={{ ...props.style }}>
       <svg
-        width="1em"
-        height="1em"
-        viewBox="0 0 12 12"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+        width='1em'
+        height='1em'
+        viewBox='0 0 12 12'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
         {...props}
       >
         <title>A Circle Icon</title>
-        <circle cx={6} cy={6} r={4} fill="#C84031" />
+        <circle cx={6} cy={6} r={4} fill='#C84031' />
         <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M6 12A6 6 0 106 0a6 6 0 000 12zm0-.857A5.143 5.143 0 106 .857a5.143 5.143 0 000 10.286z"
-          fill="#C84031"
+          fillRule='evenodd'
+          clipRule='evenodd'
+          d='M6 12A6 6 0 106 0a6 6 0 000 12zm0-.857A5.143 5.143 0 106 .857a5.143 5.143 0 000 10.286z'
+          fill='#C84031'
         />
       </svg>
     </span>
@@ -221,12 +222,12 @@ function SquareIcon(props: React.SVGProps<SVGSVGElement> & CSSProperties) {
       <svg
         width={6}
         height={6}
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
         {...props}
       >
         <title>A Square Icon</title>
-        <path fill="#fff" d="M0 0h6v6H0z" />
+        <path fill='#fff' d='M0 0h6v6H0z' />
       </svg>
     </span>
   );
@@ -259,7 +260,9 @@ const MicSelect: React.FunctionComponent<MicSelectProps> = ({
       value={value}
       onChange={onChange}
     >
-      <option value="" disabled selected hidden>Default Audio Input</option>
+      <option value='' disabled selected hidden>
+        Default Audio Input
+      </option>
       {Object.entries(options).map(([key, value]) => (
         <option value={value.value} key={key}>
           {value.label}
--- examples/microphone_nextjs/utils/auth.ts
+++ examples/microphone_nextjs/utils/auth.ts
@@ -1,20 +1,20 @@
-import { RealtimeSession } from "speechmatics";
+import { RealtimeSession } from 'speechmatics';
 
 export function getJwt() {
   // Instantiate an RT session to get the default URLs. Should really be available in their own right
   // This is a limitation of the SDK interface and will be fixed at some point
-  const rtSession = new RealtimeSession("");
+  const rtSession = new RealtimeSession('');
 
   return fetch(
     `${rtSession.connectionConfig.managementPlatformUrl}/api_keys?type=rt`,
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         Authorization: `Bearer ${process.env.MP_API_KEY}`,
       },
       body: JSON.stringify({ ttl: 3600 }),
-    }
+    },
   )
     .then((res) => res.json())
     .then((data) => data.key_value);
