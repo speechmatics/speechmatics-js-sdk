d40e9ca19d2dc50f34a50fc22401a67b6b9f6c6f
Alasdair McLeay <alasdair.mcleay@me.com>
2023-09-15 17:12:16 +0100

    useSyncExternalStore
    - Use useSyncExternalStore rather than useEffect for audio access
    - Extract MicSelect to own component to prevent hydration issues
    - Disable mic select unless in configure or block state
    - allow transcription to be started in blocked state (some browsers will reshow access dialog)
    - Disable default dropdown and with a clickable element to trigger auth dialog
    - Track audio state for devices, denied, inside a singleton class, to improve interaction via useSyncExternalStore
    - Update device list when devices are plugged in or removed
    - Provide getUserMedia from class and update devices after called

---
+++ examples/microphone_nextjs/components/MicSelect.tsx
@@ -0,0 +1,44 @@
+// MicSelect - component with a select menu for choosing mic inputs
+type Option = {
+  value: string;
+  label: string;
+};
+interface MicSelectProps {
+  value: string;
+  onChange: (event) => void;
+  onClick: () => void;
+  options: Option[];
+  disabled: boolean;
+}
+
+const MicSelect: React.FunctionComponent<MicSelectProps> = ({
+  onChange,
+  onClick,
+  options,
+  value,
+  disabled,
+}) => {
+  return options.length === 0 ? (
+    <div
+      onClick={onClick}
+      onKeyUp={onClick}
+      role='button'
+      tabIndex={0}
+      aria-label='Grant audio access'
+    >
+      <select disabled style={{ pointerEvents: 'none' }}>
+        <option>Default</option>
+      </select>
+    </div>
+  ) : (
+    <select disabled={disabled} value={value} onChange={onChange}>
+      {Object.entries(options).map(([key, value]) => (
+        <option value={value.value} key={key}>
+          {value.label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default MicSelect;
