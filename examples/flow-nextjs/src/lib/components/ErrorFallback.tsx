6c5d68a9bd1e49d8b11b4af471211eb50acbe618
Matt Nemitz <matt.nemitz@gmail.com>
2024-10-25 16:29:29 +0100

    Batch client init (#66)

--- examples/flow-nextjs/src/lib/components/ErrorFallback.tsx
+++ examples/flow-nextjs/src/lib/components/ErrorFallback.tsx
@@ -9,7 +9,9 @@ export function ErrorFallback({
     <article role="alert">
       <p>Something went wrong!</p>
       <pre>{JSON.stringify(error)}</pre>
-      <button type="reset" onClick={resetErrorBoundary}>Retry</button>
+      <button type="reset" onClick={resetErrorBoundary}>
+        Retry
+      </button>
     </article>
   );
 }
