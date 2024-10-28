d8649fd462af78060045d5318155c985d5f0b4ef
Matt Nemitz <mattn@speechmatics.com>
2024-10-28 15:22:21 +0000

    Add package for jwt fetching

--- examples/nextjs/src/app/flow/page.tsx
+++ examples/nextjs/src/app/flow/page.tsx
@@ -1,16 +1,25 @@
-import { fetchCredentials } from '@/lib/fetch-credentials';
 import Component from './Component';
 import { fetchPersonas, FlowProvider } from '@speechmatics/flow-client-react';
+import { getSpeechmaticsJWT } from '@speechmatics/jwts';
 
 export default async function Home() {
+  const apiKey = process.env.API_KEY;
+  if (!apiKey) {
+    throw new Error('Set `API_KEY` in .env file');
+  }
+
   // Credentials here are being fetched when rendering the server component.
   // You could instead define an API action to request it on the fly.
-  const creds = await fetchCredentials();
+  const jwt = await getSpeechmaticsJWT({
+    type: 'flow',
+    ttl: 120,
+    apiKey,
+  });
   const personas = await fetchPersonas();
 
   return (
     <FlowProvider appId="nextjs-example">
-      <Component jwt={creds.key_value} personas={personas} />
+      <Component jwt={jwt} personas={personas} />
     </FlowProvider>
   );
 }
---
+++ packages/jwts/src/index.ts
@@ -0,0 +1,34 @@
+export async function getSpeechmaticsJWT({
+  type,
+  apiKey,
+  clientRef,
+  ttl = 60,
+  managementPlatformURL = 'https://mp.speechmatics.com/v1',
+}: {
+  type: 'batch' | 'rt' | 'flow';
+  apiKey: string;
+  clientRef?: string;
+  ttl?: number;
+  managementPlatformURL?: string;
+}): Promise<string> {
+  if (type === 'batch' && !clientRef) {
+    throw new Error(
+      'Must set the `client_ref` parameter when using temporary keys for batch transcription. See documentation at https://docs.speechmatics.com/introduction/authentication#batch-transcription',
+    );
+  }
+
+  const resp = await fetch(`${managementPlatformURL}/api_keys?type=${type}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      ttl,
+    }),
+  });
+  if (!resp.ok) {
+    throw new Error('Bad response from API', { cause: resp });
+  }
+  return (await resp.json()).key_value;
+}
