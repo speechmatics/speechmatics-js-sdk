489db93240d24635374b228d31d1c993dce92491
Matt Nemitz <mattn@speechmatics.com>
2024-10-31 11:54:02 +0000

    Export all model types from batch and RT client packages

--- packages/batch-client/scripts/generate-models.sh
+++ packages/batch-client/scripts/generate-models.sh
@@ -3,3 +3,12 @@
 PROJECT_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/..
 
 openapi-generator generate -i ${PROJECT_ROOT}/schema/batch.yml -g typescript-axios --global-property models -o ${PROJECT_ROOT}/ -c ${PROJECT_ROOT}/schema/autogen.json
+
+cd $PROJECT_ROOT/models;
+
+rm -f index.ts;
+
+for tsfile in ./*.ts; do
+    module_name=$(echo "$tsfile" | sed -e 's/\.ts$//')
+    echo "export * from '${module_name}';" >> index.ts
+done
