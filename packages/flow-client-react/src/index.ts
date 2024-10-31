ea6cd4d5fccf2a0bea2d1dde090777b2b59c0968
Matt Nemitz <mattn@speechmatics.com>
2024-10-31 11:55:51 +0000

    Export all flow-client values from flow-client-react for convenience

--- packages/flow-client-react/src/index.ts
+++ packages/flow-client-react/src/index.ts
@@ -2,4 +2,4 @@ export * from './use-flow';
 export * from './use-flow-event-listener';
 export * from './flow-context';
 export * from './flow-provider';
-export { fetchPersonas } from '@speechmatics/flow-client';
+export * from '@speechmatics/flow-client';
