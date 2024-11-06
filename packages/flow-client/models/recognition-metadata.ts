d04c6b59565fddd9973537998e4805d4d49fe118
Tudor Evans <104087420+TudorCRL@users.noreply.github.com>
2024-11-06 14:39:35 +0000

    Add transcript message in flow client (#81)
    Add transcript message. Copy across types from realtime client for now.

---
+++ packages/flow-client/models/recognition-metadata.ts
@@ -0,0 +1,27 @@
+// NOTE: This file is copied from realtime-client, not auto-generated
+
+/**
+ *
+ * @export
+ * @interface RecognitionMetadata
+ */
+export interface RecognitionMetadata {
+  /**
+   *
+   * @type {number}
+   * @memberof RecognitionMetadata
+   */
+  start_time: number;
+  /**
+   *
+   * @type {number}
+   * @memberof RecognitionMetadata
+   */
+  end_time: number;
+  /**
+   *
+   * @type {string}
+   * @memberof RecognitionMetadata
+   */
+  transcript: string;
+}
--- packages/flow-client/src/index.ts
+++ packages/flow-client/src/index.ts
@@ -1,3 +1,4 @@
 export * from './client';
 export * from './events';
 export * from './personas';
+export * from '../models';
