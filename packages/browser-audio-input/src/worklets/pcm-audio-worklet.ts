89b287f9e0985f4b82f4ba9f2a2b5c2635918941
Matt Nemitz <mattn@speechmatics.com>
2024-11-27 13:28:59 +0000

    Clean up worklet code

--- packages/browser-audio-input/src/worklets/pcm-audio-worklet.ts
+++ packages/browser-audio-input/src/worklets/pcm-audio-worklet.ts
@@ -1,7 +1,6 @@
 class PcmAudioProcessor extends AudioWorkletProcessor {
-  process(inputs: Float32Array[][], outputs: Float32Array[][]) {
+  process(inputs: Float32Array[][]) {
     const input = inputs[0];
-    const output = outputs[0];
 
     if (input.length > 0) {
       const inputBuffer = input[0];
