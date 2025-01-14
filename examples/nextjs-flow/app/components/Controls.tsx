222e1fbae03ac542a6498ba6113475af03f46078
Bella Taylor <bella.taylor@speechmatics.com>
2025-01-14 16:09:22 +0000

    Move browserAudio hook to separate file

--- examples/nextjs-flow/app/components/Controls.tsx
+++ examples/nextjs-flow/app/components/Controls.tsx
@@ -1,18 +1,8 @@
 'use client';
-import { type FormEventHandler, useCallback, useMemo, useState } from 'react';
-import {
-  type AgentAudioEvent,
-  useFlow,
-  useFlowEventListener,
-} from '@speechmatics/flow-client-react';
+import { useFlow } from '@speechmatics/flow-client-react';
+import { useCallback, useMemo, type FormEventHandler } from 'react';
+import { useFlowWithBrowserAudio } from '../hooks/useFlowWithBrowserAudio';
 import { MicrophoneSelect, Select } from './MicrophoneSelect';
-import { getJWT } from '../actions';
-import {
-  usePcmAudioListener,
-  usePcmAudioRecorder,
-} from '@speechmatics/browser-audio-input-react';
-import { RECORDING_SAMPLE_RATE } from '../lib/constants';
-import { usePlayPcm16Audio } from '../lib/audio-hooks';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
@@ -91,62 +81,4 @@ export function Controls({
       </form>
     </div>
   );
-}
-
-// Hook to set up two way audio between the browser and Flow
-function useFlowWithBrowserAudio() {
-  const { startConversation, endConversation, sendAudio } = useFlow();
-  const { startRecording, stopRecording } = usePcmAudioRecorder();
-  const [audioContext, setAudioContext] = useState<AudioContext>();
-  const playAudio = usePlayPcm16Audio(audioContext);
-
-  // Send audio to Flow when we receive it from the active input device
-  usePcmAudioListener(sendAudio);
-
-  // Play back audio when we receive it from flow
-  useFlowEventListener(
-    'agentAudio',
-    useCallback(
-      ({ data }: AgentAudioEvent) => {
-        playAudio(data);
-      },
-      [playAudio],
-    ),
-  );
-
-  const startSession = useCallback(
-    async ({
-      personaId,
-      deviceId,
-    }: { personaId: string; deviceId: string }) => {
-      const jwt = await getJWT('flow');
-
-      await startConversation(jwt, {
-        config: {
-          template_id: personaId,
-          template_variables: {},
-        },
-        audioFormat: {
-          type: 'raw',
-          encoding: 'pcm_f32le',
-          sample_rate: RECORDING_SAMPLE_RATE,
-        },
-      });
-
-      const audioContext = new AudioContext({
-        sampleRate: RECORDING_SAMPLE_RATE,
-      });
-      setAudioContext(audioContext);
-
-      await startRecording({ deviceId, sampleRate: RECORDING_SAMPLE_RATE });
-    },
-    [startConversation, startRecording],
-  );
-
-  const stopSession = useCallback(async () => {
-    endConversation();
-    stopRecording();
-  }, [endConversation, stopRecording]);
-
-  return { startSession, stopSession };
 }
---
+++ examples/nextjs-flow/app/hooks/useFlowWithBrowserAudio.ts
@@ -0,0 +1,72 @@
+'use client';
+import { useCallback, useState } from 'react';
+import {
+  type AgentAudioEvent,
+  useFlow,
+  useFlowEventListener,
+} from '@speechmatics/flow-client-react';
+import { getJWT } from '../actions';
+import {
+  usePcmAudioListener,
+  usePcmAudioRecorder,
+} from '@speechmatics/browser-audio-input-react';
+import { RECORDING_SAMPLE_RATE } from '../lib/constants';
+import { usePlayPcm16Audio } from '../lib/audio-hooks';
+
+// Hook to set up two way audio between the browser and Flow
+export function useFlowWithBrowserAudio() {
+  const { startConversation, endConversation, sendAudio } = useFlow();
+  const { startRecording, stopRecording } = usePcmAudioRecorder();
+  const [audioContext, setAudioContext] = useState<AudioContext>();
+  const playAudio = usePlayPcm16Audio(audioContext);
+
+  // Send audio to Flow when we receive it from the active input device
+  usePcmAudioListener(sendAudio);
+
+  // Play back audio when we receive it from flow
+  useFlowEventListener(
+    'agentAudio',
+    useCallback(
+      ({ data }: AgentAudioEvent) => {
+        playAudio(data);
+      },
+      [playAudio],
+    ),
+  );
+
+  const startSession = useCallback(
+    async ({
+      personaId,
+      deviceId,
+    }: { personaId: string; deviceId: string }) => {
+      const jwt = await getJWT('flow');
+
+      await startConversation(jwt, {
+        config: {
+          template_id: personaId,
+          template_variables: {},
+        },
+        audioFormat: {
+          type: 'raw',
+          encoding: 'pcm_f32le',
+          sample_rate: RECORDING_SAMPLE_RATE,
+        },
+      });
+
+      const audioContext = new AudioContext({
+        sampleRate: RECORDING_SAMPLE_RATE,
+      });
+      setAudioContext(audioContext);
+
+      await startRecording({ deviceId, sampleRate: RECORDING_SAMPLE_RATE });
+    },
+    [startConversation, startRecording],
+  );
+
+  const stopSession = useCallback(async () => {
+    endConversation();
+    stopRecording();
+  }, [endConversation, stopRecording]);
+
+  return { startSession, stopSession };
+}
