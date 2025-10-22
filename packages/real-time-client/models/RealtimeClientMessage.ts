02d2750a5db53ca8723b36e31851347a9044e056
Tudor Evans <tudore@speechmatics.com>
2025-10-22 13:56:37 +0100

    add force end of utterance types

--- packages/real-time-client/models/RealtimeClientMessage.ts
+++ packages/real-time-client/models/RealtimeClientMessage.ts
@@ -2,6 +2,7 @@ import type { StartRecognition } from './StartRecognition';
 import type { AddChannelAudio } from './AddChannelAudio';
 import type { EndOfStream } from './EndOfStream';
 import type { EndOfChannel } from './EndOfChannel';
+import type { ForceEndOfUtterance } from './ForceEndOfUtterance';
 import type { SetRecognitionConfig } from './SetRecognitionConfig';
 import type { GetSpeakers } from './GetSpeakers';
 type RealtimeClientMessage =
@@ -9,6 +10,7 @@ type RealtimeClientMessage =
   | AddChannelAudio
   | EndOfStream
   | EndOfChannel
+  | ForceEndOfUtterance
   | SetRecognitionConfig
   | GetSpeakers;
 export type { RealtimeClientMessage };
