dacb07bb81c8dab901e40af726c421220628209e
Matt Nemitz <mattn@speechmatics.com>
2026-05-07 17:14:04 +0100

    Revert RT changes

--- packages/real-time-client/models/ForceEndOfUtterance.ts
+++ packages/real-time-client/models/ForceEndOfUtterance.ts
@@ -4,9 +4,5 @@ interface ForceEndOfUtterance {
    * The channel to request finalized transcript from. This field is only seen in multichannel.
    */
   channel?: string;
-  /**
-   * Timestamp of the audio data that corresponds to the force end of utterance request. It's the number of seconds since the beginning of the audio.
-   */
-  timestamp?: number;
 }
 export type { ForceEndOfUtterance };
