367ffb1a55da8f0742247014c1f26831da199e7a
Matt Nemitz <mattn@speechmatics.com>
2026-07-20 11:37:51 +0100

    Fix `getJWT` for RT

--- examples/nextjs-real-time-transcription/app/actions.ts
+++ examples/nextjs-real-time-transcription/app/actions.ts
@@ -2,11 +2,11 @@
 
 import { createSpeechmaticsJWT } from '@speechmatics/auth';
 
-export async function getJWT(type: 'flow' | 'rt') {
+export async function getJWT() {
   const apiKey = process.env.SPEECHMATICS_API_KEY;
   if (!apiKey) {
     throw new Error('Please set the API_KEY environment variable');
   }
 
-  return createSpeechmaticsJWT({ type, apiKey, ttl: 60 });
+  return createSpeechmaticsJWT({ type: 'rt', apiKey, ttl: 60 });
 }
